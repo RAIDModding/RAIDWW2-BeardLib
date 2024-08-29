02695b1b4b7058e50b289a8982ad5983d29affc8
BangL <henno.rickowski@gmail.com>
2024-08-29 02:46:18 +0200

    why start with deprecation?

--- Classes/Frameworks.lua
+++ Classes/Frameworks.lua
@@ -330,16 +330,6 @@ MapFramework._ignore_folders = {backups = true, prefabs = true}
 MapFramework._directory = BeardLib.config.maps_dir
 MapFramework.type_name = "Map"
 
-function MapFramework:init()
-    -- Deprecated, try not to use.
-    if self.type_name == MapFramework.type_name then
-        BeardLib.Frameworks.map = self
-        BeardLib.managers.MapFramework = self
-    end
-
-    MapFramework.super.init(self)
-end
-
 ---@deprecated
 ---Use BeardLib.Utils:GetMapByJobId instead
 function MapFramework:GetMapByJobId(job_id)
