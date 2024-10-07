236ed6b9e6dbe5536db2073389c2c66e4091fec7
BangL <henno.rickowski@gmail.com>
2024-10-07 22:26:10 +0200

    removed legacy code

--- Modules/Utils/KeybindModule.lua
+++ Modules/Utils/KeybindModule.lua
@@ -9,9 +9,5 @@ function KeybindModule:Load()
 	self._config.keybind_id = self._config.keybind_id or self._config.id
 	local config = table.merge({run_in_menu = true, run_in_game = true}, self._config)
 
-	if BLT and BLT.Keybinds then
-		BLT.Keybinds:register_keybind_json(self._mod, config)
-	else
-		LuaModManager:AddJsonKeybinding(config, self._mod.ModPath .. "/")
-	end
+	BLT.Keybinds:register_keybind_json(self._mod, config)
 end
